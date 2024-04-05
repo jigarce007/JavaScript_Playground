780e7635dcb36ec47c471b9aa78d28d2fa0766ef
Jigar <jigararvindbhai.makwana@maybank.com>
2024-04-05 23:33:37 +0800

    practice lesson for variables

---
+++ letVarConst.js
@@ -0,0 +1,20 @@
+//Lesson for declaring and assigning values to variables
+// THREE WAYS let,var and const
+
+//let is commonand mutable.. can change the value of it when it's required in future.
+let fName
+fName = 'Jigar'
+
+//const for constant value ... can not be change in future once asigned the value.
+const birthyear = 1991
+
+//var is similar to 'let' but there is differnce of "function specific and block specific"
+var lName;
+lName = "Makwana"
+
+console.log(fName)
+console.log(lName)
+console.log(birthyear)
+
+
+
