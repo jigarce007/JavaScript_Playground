d9218b04663bb5f8a679158a371938129a692091
Jigar <jigararvindbhai.makwana@maybank.com>
2024-04-05 22:49:14 +0800

    Practice assignement on variables and values

--- script.js
+++ script.js
@@ -5,5 +5,15 @@ let lastName = "Makwana"
 
 console.log("Welcome ",firstName) 
 
+//practice Assignment on Variables and Constants
+//PRBLEM STATEMENT :    Declare variables called country, continent and population and assign their values according to your own country (population in millions).
+//Log their values to the console
 
+//SOLUTION
+let country = "INDIA"
+let continent = "ASIA"
+let population = 100
 
+console.log(country)
+console.log(continent)
+console.log(population)
