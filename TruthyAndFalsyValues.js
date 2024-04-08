3bf586a8675421f5d67eb5f3e4e69d9220934d2f
Jigar <jigararvindbhai.makwana@maybank.com>
2024-04-08 22:16:49 +0800

    Type conversion,coersion & Truthy&FAlsy values

--- TruthyAndFalsyValues.js
+++ TruthyAndFalsyValues.js
@@ -16,3 +16,12 @@ console.log(Boolean(null))
 //other than the above values are considered as truthy values when we convert it to boolean..example
 console.log(Boolean(1)) // will be true
 console.log(Boolean('Null')) //'Null' is a strig here
+
+
+// we can use these directly in IF else too..
+const money = 0
+if (money) {
+    console.log('Spend some money on friends!')
+} else {
+    console.log('You should start a job!')
+}
